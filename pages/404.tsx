3fbdc0c9e01cb22243f2400ed11675140ba403de
Alexander Borminskij <alexander.borminskij@interactivesolutions.se>
2022-05-31 17:10:26 +0200

    Added counter and redirect to home

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { Container, Center, Box, Circle } from "@chakra-ui/react";
 
 const NotFound = () => {
+  const router = useRouter();
+  const [counter, setCounter] = useState(3);
+
+  useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  });
+
   return (
     <>
       <Container maxW="container.lg" mt={10}>
@@ -9,7 +20,7 @@ const NotFound = () => {
           <Box>
             <Center>
               <Circle size="40px" bg="purple.500" color="white">
-                404
+                {counter}
               </Circle>
             </Center>
           </Box>
