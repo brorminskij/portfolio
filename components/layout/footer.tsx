4ab6b60eada3b45acfd6c3740c7e2978436ae8f6
Alexander Borminskij <alexander.borminskij@interactivesolutions.se>
2022-07-11 14:13:09 +0200

    imports

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,3 +1,17 @@
+import {
+  Box,
+  Stack,
+  Text,
+  Container,
+  ButtonGroup,
+  IconButton,
+  Link,
+  Divider,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import logo from "/public/wolf2.png";
+
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 const Footer = () => (
 );
 
