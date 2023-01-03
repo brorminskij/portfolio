52623570a94d7114e1f9da49de9ab8895e5d492a
Alexander Borminskij <alexander.borminskij@interactivesolutions.se>
2023-01-04 00:42:51 +0100

    Fixed build issues

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,5 +1,53 @@
-export const RecipeDetails = () => {
-  return <div>Recipe Details</div>;
+import { createClient } from "contentful";
+import Image from "next/image";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "article",
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "article",
+    "fields.slug": params.slug,
+  });
+  return {
+    props: { article: items[0] },
+  };
+}
+
+export const RecipeDetails = ({ article }) => {
+  const { featuredImage, title } = article.fields;
+  return (
+    <div>
+      <div className="banner">
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+          alt="Featured image fetched from contentful"
+        />
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeDetails;
